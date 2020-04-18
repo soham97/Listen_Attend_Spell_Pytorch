5f36a9a80f7afe532f00f5ce5ea5377af53a6feb
soham97 <soham97@yahoo.co.in>
2020-04-17 23:24:15 -0400

    embed size corrected

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
-python3 main.py -data_dir data -hidden_dim 256  -embed_dim 100 \
+python3 main.py -data_dir data -hidden_dim 256  -embed_dim 40 \
 -batch_size 32 -epochs 50 -lr 0.001 -clip_value 0.0 -w_decay 0.0 \
 -max_decoding_length 300 -is_stochastic 1 -train 1 -models_dir models \
 -logs_dir logs -model_path best.pth -num_workers 64
