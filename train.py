71ef49d57e37df797f41a6a101015ded8dc1e536
soham97 <soham97@yahoo.co.in>
2020-04-17 23:57:40 -0400

    perplexity added

--- train.py
+++ train.py
@@ -27,7 +27,7 @@ def train(args, logging, cuda):
         train_loss_samples = []
         val_loss_samples = []
         model.train()
-        for (x, x_len, y, y_len, y_mask) in tqdm(DataLoaderContainer.train_dataloader):
+        for (x, x_len, y, y_len, y_mask) in tqdm(DataLoaderContainer.val_dataloader):
             if cuda:
                 x = x.cuda()
                 y = y.cuda()
@@ -72,7 +72,8 @@ def train(args, logging, cuda):
             best_val_loss = val_loss
             save_model(epoch, model, optimizer, scheduler, model_path)
 
-        logging.info('epoch: {}, train_loss: {:.3f}, val_loss: {:.3f}'.format(epoch, train_loss, val_loss))
+        logging.info('epoch: {}, train_loss: {:.3f}, train_perplexity: {:.3f}, \
+                    val_loss: {:.3f}, val_perplexity: {:.3f}'.format(epoch, train_loss, np.exp(train_loss), val_loss, np.exp(val_loss)))
     
     
     return model
